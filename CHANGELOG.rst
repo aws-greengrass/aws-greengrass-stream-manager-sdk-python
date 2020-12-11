009c07089e4a1f3522fd4c0c76ef5ace3e026ee5
sushantAtAws <shsush@amazon.com>
2020-12-11 03:22:59 +0000

    Release of Version 1.1.0

---
+++ CHANGELOG.rst
@@ -0,0 +1,7 @@
+=========
+CHANGELOG
+=========
+
+1.1.0
+=====
+Stream manager supports automatic data export to AWS S3 and AWS IoT SiteWise, provides new API method to update existing streams, and pause or resume exporting.
---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""
+distutils/setuptools install script.
+"""
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+ROOT = os.path.dirname(__file__)
+VERSION_RE = re.compile(r'''SDK_VERSION = ['"]([0-9.]+)['"]''')
+
+
+requires = [
+    "cbor2==4.1.2"
+]
+
+
+def get_version():
+    init = open(os.path.join(ROOT,'stream_manager','streammanagerclient.py')).read()
+    return VERSION_RE.search(init).group(1)
+
+
+setup(
+    name='stream_manager',
+    version=get_version(),
+    description='The AWS IoT Greengrass Stream Manager SDK for Python',
+    long_description=open('README.rst').read(),
+    author='Amazon Web Services',
+    url='',
+    scripts=[],
+    packages=find_packages(),
+    package_data={
+        'stream_manager': [
+        ]
+    },
+    include_package_data=True,
+    install_requires=requires,
+    license="Apache License 2.0",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
